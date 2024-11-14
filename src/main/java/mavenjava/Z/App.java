19e406eda99fb36c11621065f978ed1481fcf253
Vyahruti <vyahrutechnical4@gmail.com>
2024-11-14 15:37:11 +0530

    maven-java

---
+++ src/main/java/mavenjava/Z/App.java
@@ -0,0 +1,17 @@
+package mavenjava.Z;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        int a=10;
+        int b=30;
+        int c=a+b;
+        System.out.println("The sum is "+c);
+    }
+}
